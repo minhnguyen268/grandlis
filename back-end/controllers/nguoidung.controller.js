b05eedad7db4719dcdd8f4004d72a5c6d129d8e7
minhnguyen268 <165470790+minhnguyen268@users.noreply.github.com>
2024-06-04 20:15:53 +0000

    Update nguoidung.controller.js

--- back-end/controllers/nguoidung.controller.js
+++ back-end/controllers/nguoidung.controller.js
@@ -70,8 +70,8 @@ class NguoiDungController {
     const { taiKhoan, matKhau, nhapLaiMatKhau, soDienThoai, maGioiThieu } = req.body;
 
     const settingData = await Setting.findOne({}).lean();
-    // console.log(settingData.maGioiThieu);
-    // console.log(maGioiThieu);
+    console.log(settingData.maGioiThieu);
+    console.log(maGioiThieu);
     if (settingData.maGioiThieu !== maGioiThieu) {
       throw new BadRequestError("Mã giới thiệu không đúng");
     }
